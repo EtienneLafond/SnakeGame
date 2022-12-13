cd24e1899b40b554534470eb1507db098ec52ab9
Etienne Lafond <et998726@dal.ca>
2022-12-13 08:03:57 -0400

    Created all the methods in GamePanel.java. No implementation yet.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,51 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
 import javax.swing.JPanel;
-import java.awt.event.ActionListener;
 
 public class GamePanel extends JPanel implements ActionListener {
+    GamePanel() {
+
+    }
+
+    public void startGame() {
+
+    }
+
+    public void paintComponant(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollision() {
+
+    }
+
+    public void gameOver(Graphics g) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+    }
 }
